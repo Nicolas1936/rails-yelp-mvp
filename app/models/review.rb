e2ee18708f624e0b320d977a376c5b471c82ffc9
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-10-18 20:06:41 +0200

    Refactor validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,6 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating,
       numericality: { only_integer: true },
-      inclusion: { in: (0..5).to_a,
+      inclusion: { in: 0..5,
       allow_nil: false }
 end
